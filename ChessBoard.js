09f9a85aab450ecc33be60d971fe6986ba93ff1a
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2021-03-03 09:51:52 +0300

    Create ChessBoard.js

---
+++ ChessBoard.js
@@ -0,0 +1,16 @@
+var chessWrap = document.querySelector('.ch-wrap');
+
+var i = 0, count = 0;
+
+while (count < 8 * 8) {
+  var item = document.createElement('div');
+  chessWrap.appendChild(item);
+
+  item.classList.add('ch-item');
+
+  if (i && i % 2)
+    item.classList.add('ch-black')
+
+  i += ((i + 2) % 9) ? 1 : 2;
+  count++;
+}
